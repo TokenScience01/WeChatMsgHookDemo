afe3e48171272966b291bd30fc82e78303356b66
jionecc <995000812@qq.com>
2018-06-04 22:29:39 +0800

    Hook微信消息并且监听

---
+++ app/src/main/java/HookMain.java
@@ -0,0 +1,51 @@
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * = █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * <p>
+ * ---------------------------Created by Xyx on 2018/6/4.----------------
+ */
+
+public class HookMain implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpp) throws Throwable {
+
+        if (lpp.packageName.contains("com.tencent.mm")){
+            XposedBridge.log("====进入微信了====");
+
+            XposedHelpers.findAndHookMethod("com.tencent.mm.ui.chatting.o",
+                    lpp.classLoader,
+                    "EM",
+                    String.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            super.beforeHookedMethod(param);
+                            String arg = (String) param.args[0];
+
+                            XposedBridge.log("arg:"+arg);
+
+                            if (arg.equals("Test")){
+                                param.setResult((true));
+                            }
+
+
+                        }
+                    }
+            );
+        }
+
+    }
+}
